da63bf87097f4487683528520451baa82c6bd34c
yydeng <dengyy@cartechfin.com>
2019-05-01 22:02:46 +0800

    init

---
+++ src/main/java/com/cecgw/cq/trafficsys/trafficsyscustomer/TrafficsysCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.cecgw.cq.trafficsys.trafficsyscustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficsysCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficsysCustomerApplication.class, args);
+    }
+
+}
